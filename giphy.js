d376ade5c8bf190295be014c15a50e4831c92532
mariamgulbani <mgulbani1@gmail.com>
2022-08-26 22:32:56 +0400

    fetch fix

--- giphy.js
+++ giphy.js
@@ -2,28 +2,22 @@ import { Base } from "./base.js";
 export class Giphy extends Base {
   constructor(res) {
     super();
-     this.res = res;
+    this.res = res;
   }
 
-//get products
-getGiphy() {
-    //console.log(obj.url)
-let list = this.res.data.map((obj) => {console.log("kkk",obj)
-console.log('button', document.getElementsByClassName("btn-add").value)
+  //get gifs
+  getGiphy() {
+    let list = this.res.data.map((obj) => {
+      console.log("button", document.getElementsByClassName("btn-add").value);
 
-return `<img class="img" src=${obj.images.fixed_height.url}>`
+      return `<img class="img" src=${obj.images.fixed_height.url}>`;
+    });
 
-});
+    return list.join("");
+    return list;
+  }
 
-
-return list.join("");
-return list;
-
-
+  render() {
+    this.setContent("items", this.getGiphy());
+  }
 }
-
-render() {
-    this.setContent('items', this.getGiphy());
-    console.log(document.getElementById('items'))
-    
-}}
--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import { Giphy } from "./giphy.js";
 import { baseUrl } from "./config.js";
-import{key} from"./config.js"
-
-
+import { key } from "./config.js";
 
 //trending sort
 fetch(`${baseUrl}trending?${key}`)
@@ -10,76 +8,46 @@ fetch(`${baseUrl}trending?${key}`)
     return res.json();
   })
   .then((res) => {
-     console.log("res", res);
-    
-
     let giphyPage = new Giphy(res);
     function setC() {
       giphyPage.render();
     }
     document.getElementById("trend").addEventListener("click", setC);
-    
   });
 
- //buttons sort
+//buttons sort
 
 let elem = document.getElementsByClassName("btn-add");
-for (let i = 0; i < elem.length; i++){
-  console.log(elem[i])
+for (let i = 0; i < elem.length; i++) {
   let arr = elem[i];
-  arr.addEventListener('click', ()=> {
-    for (let j = 0; j < elem.length; j++){
-      elem[j].classList.remove('active');
+  arr.addEventListener("click", () => {
+    for (let j = 0; j < elem.length; j++) {
+      elem[j].classList.remove("active");
     }
-    arr.classList.add('active');
-    console.log("act",document.activeElement.value)
+    arr.classList.add("active");
     fetch(`${baseUrl}search?q=${document.activeElement.value}&${key}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        let giphyPage = new Giphy(res);
+        giphyPage.render();
+      });
+  });
+}
+//search sort
+let val = document.getElementById("user-search").value;
+function getVal() {
+  val = document.getElementById("user-search").value;
+
+  fetch(`${baseUrl}search?q=${val}&${key}`)
     .then((res) => {
       return res.json();
     })
     .then((res) => {
-  
       let giphyPage = new Giphy(res);
-      function setC() {
-        giphyPage.render();
-      }
-      elem[i].addEventListener('click',setC)
-    }
-    );
-  })
-
-
-}
-
-let val;
-    function getVal() {
-       val = document.getElementById('user-search').value;
-      console.log(val);
-    
-fetch(`${baseUrl}search?q=${val}&${key}`)
-  .then((res) => {
-    return res.json();
-  })
-  .then((res) => {
-     console.log("res", res);
-    
-
-    let giphyPage = new Giphy(res);
-    function setC() {
       giphyPage.render();
-    }
-    console.log(val);
-
-  
-    document.getElementById("submit").addEventListener("click", setC);
-    
-
-    console.log("mmm",document.getElementById('user-search').value)
-  });
+    });
 }
+
 document.getElementById("submit").addEventListener("click", getVal);
-
-
-
-
-
